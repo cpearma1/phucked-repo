c65cda88c9628b8288ff0cebb35eb8760c7c8253
Caleb <caleb.a.pearman@gmail.com>
2021-03-31 20:05:04 -0400

    got rid of extra database and tried to debug the controller. Enabled debugging.

--- build/resources/main/data.sql
+++ build/resources/main/data.sql
@@ -1,4 +1,5 @@
-MERGE INTO CLASS_PAGE VALUES
+
+INSERT INTO CLASS_PAGE VALUES
 (1, 'Bojan', 'Cukic','ITSC-4155'),
 (2, 'Min','Shin','ITCS-4152/5152'),
 (3, 'Sterling','McLeod','ITCS-4151'),
--- src/main/java/com/controller/SearchController.java
+++ src/main/java/com/controller/SearchController.java
@@ -2,6 +2,8 @@ package com.controller;
 
 import com.group12.FastLeaning.ClassPage;
 import com.service.ClassPageService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
@@ -10,10 +12,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class SearchController {
+    Logger logger = LoggerFactory.getLogger(SearchController.class);
+
 
     @Autowired
     private ClassPageService classPageService;
@@ -23,12 +28,18 @@ public class SearchController {
         return "index";
     }
 
+
     @RequestMapping("/")
     public String searchSubmit(Model model, @Param("query") String query){
         List<ClassPage> classPages = classPageService.findBySearchQuery(query);
-        model.addAttribute("ClassPages", classPages);
+        logger.debug(Arrays.toString(classPages.toArray()));
+        logger.warn(Arrays.toString(classPages.toArray()));
+        logger.error(Arrays.toString(classPages.toArray()));
+        logger.trace(Arrays.toString(classPages.toArray()));
+        model.addAttribute("classPages", classPages);
         model.addAttribute("query", query);
         return "index";
-
     }
+
+
 }
